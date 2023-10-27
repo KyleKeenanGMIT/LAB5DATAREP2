45e4d7015473559e98e4593c4804efddc9adfc29
KyleKeenanGmit <kylekeenan@live.co.uk>
2023-10-27 09:37:21 +0000

    files finished

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 4000//will see example app is listening on port 4000 when running the code. - localhost:4000 in browser to view.
+const path = require('path');//path is another libary
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());//body parser installed using the cmd line npm install body-parser.
+
+app.post('/name', (req, res) =>{
+    res.send('Hello ' +req.body.fname);//post method.
+})
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to Data Representation & Querying')
@@ -14,7 +24,17 @@ res.send('hello '+ req.params.name);
 })//localhost:4000/hello will show the res.send. - /:name adds the parameter.
 //when running for the parameter you will type a name eg. localhost:4000/hello/kyle - returns hello kyle on the screen.
 
-app.get('/api/books', (req,res) => {
+app.get('/test', (req,res) =>{
+    res.sendFile(__dirname + '/index.html')//dirname will get me current directory and grab index.html from there. - returns data from the html file.
+})
+
+
+
+app.get('/name', (req,res) =>{
+    res.send("Hello "+req.query.fname+" "+ req.query.lname) //http://localhost:4000/name?fname=kyle&lname=keenan url after pressing submit.
+})
+
+app.get('/api/books', (req, res) => {
     const data = [
         {
         "title": "Learn Git in a Month of Lunches",
@@ -50,12 +70,14 @@ app.get('/api/books', (req,res) => {
         "categories": []
         }
         ];//array of data created.
-    res.json({
+    res.status(200).json({//200 means everything has happened sucessfully. - 500 = internal server error.
         myBooks:data,//new array called mybooks = data.
         "message": "Hello from the server"
     });//to run - localhost:4000/api/books. - shows the data from the res.json
 }
 )
+
+
 
 app.get('/whatever', (req,res) => {
     res.send('Goodbye');
